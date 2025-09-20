5f737ea1d4d4002b8e188788e8f2bacffd21bcb4
Athul S <athul162004@gmail.com>
2025-09-20 15:52:22 +0530

    .

--- AVR/DELAY_TIMER0/DELAY_TIMER0/main.c
+++ AVR/DELAY_TIMER0/DELAY_TIMER0/main.c
@@ -5,34 +5,32 @@
  * Author : KICHU
  */ 
 
+
 #define F_CPU 16000000UL
 #include <avr/io.h>
 
-void delay_1s(void)
-{
-	unsigned int i, count = 61;
-	for(i = 0;i<count;i++) //This will run for 61 times for getting 1sec delay (1 sec / 16.384ms = 61 times)
-	{
-		TCNT0 = 0;
-		TCCR0A = 0; // FOR NORMAL CLOCK SOURCE
-		TCCR0B |= (1 << CS02) | (1 << CS00); //101 FOR 1024 PRESCALING,SO SET CS02 and CS02 TO 1.(16Mhz/1024 = 15.625Khz)
-		TIFR0 |= (1 << TOV0);  //SET OVERFLOW FLAG 0 BY WRITING 1 TO IT.
-		while (!(TIFR0 & (1 << TOV0))); //WAITING TIMER OVERFLOW FLAG TO SET SO THAT WE GET 16.384ms DELAY.
+void delay_1s(void){
+	unsigned int i;
+	unsigned int count=976;
+	for(i=0; i<count; i++){
+		TCNT0=0;
+		TCCR0A=0;
+		TCCR0B|=(1<<CS00);
+		TCCR0B|=(1<<CS01);
+		TIFR0|=(1<<TOV0);
+		
+		while(!(TIFR0 & (1<<TOV0)));
 	}
 }
-
-
-
 int main(void)
 {
-	
-	DDRB |= (1 << DDB5);
-	
+	DDRB|=(1<<DDB5);
 	while (1)
 	{
-		PORTB ^= (1 << PORTB5);  // FOR BLINKING LED.
+		PORTB^=(1<<PORTB5);
 		delay_1s();
-		
+
 	}
 }
+
 
